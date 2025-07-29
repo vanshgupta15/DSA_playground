d7daf0a7c2cb50aa8b76610631b8bd53c4c07472
Vansh Gupta <vanshsja1512@gmail.com>
2025-07-29 15:46:51 +0530

    Create Student.c

---
+++ dsa/array/Student.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+//student structure
+struct Student
+{
+    int rollNo;
+    char name[50];
+    float cgpa;
+};
+
+void main()
+{
+    struct Student student[3];
+    //scanf("%s", employees[i].name);
+}
