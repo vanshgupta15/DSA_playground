f50dd92312dfe9033458d34d574245d2397b26a4
Vansh Gupta <vanshsja1512@gmail.com>
2025-08-29 15:35:44 +0530

    ll insertion at end

---
+++ dsa/linked list/InsertionEnd.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void printing(struct node *ptr)
+{
+    while(ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr= ptr->next;
+    }
+    printf("NULL");
+}
+
+void insertionAtEnd(struct node *ptr,int data)
+{
+    while(ptr->next!=NULL)
+    {
+        ptr=ptr->next;
+    }
+    struct node *addedNode=(struct node *)malloc(sizeof(struct node));
+    addedNode->data=data;
+    addedNode->next=NULL;
+    ptr->next=addedNode;
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of nodes you want to create: ");
+    scanf("%d",&n);
+    struct node *head= NULL, *newNode= NULL, *current= NULL;
+    for(int i=0; i<n; i++)
+    {
+        int data;
+        printf("Enter data for %d node: ",i+1);
+        scanf("%d",&data);
+        newNode= (struct node*)malloc(sizeof(struct node));
+        newNode->data=data;
+        newNode->next=NULL;
+        if(head==NULL)
+        {
+            head=newNode;
+            current=newNode;
+        }
+        else
+        {
+            current->next=newNode;
+            current=newNode;
+        }
+    }
+    int data;
+    printf("Enter the data you want to insert at the end: ");
+    scanf("%d",&data);
+    insertionAtEnd(head,data);
+    printing(head);
+}
