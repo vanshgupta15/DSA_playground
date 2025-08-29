e8168c49de1d0ef7fc224174450834ec9d4e6642
Vansh Gupta <vanshsja1512@gmail.com>
2025-08-30 00:31:53 +0530

    LL reversal

---
+++ dsa/linked list/Reversal.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void printing(struct node *ptr)
+{
+    while(ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");
+}
+
+struct node* reversal(struct node *ptr)
+{
+    struct node *prev=NULL, *current=ptr, *next=NULL;
+    while(current!=NULL)
+    {
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+    }
+    return prev;
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of nodes you want to create: ");
+    scanf("%d",&n);
+    struct node *head= NULL, *newNode= NULL, *current= NULL;
+    for(int i=0; i<n; i++)
+    {
+        int data;
+        printf("Enter data for %d node: ",i+1);
+        scanf("%d",&data);
+        newNode=(struct node*)malloc(sizeof(struct node));
+        newNode->data=data;
+        newNode->next=NULL;
+        if(head==NULL)
+        {
+            head=newNode;
+            current=newNode;    
+        }
+        else
+        {
+            current->next=newNode;
+            current=newNode;
+        }
+    }
+    printf("Old linked list:- \n");
+    printing(head);
+    printf("New linked list:- \n");
+    head=reversal(head);
+    printing(head);
+}
