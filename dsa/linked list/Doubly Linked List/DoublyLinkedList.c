f718d75cf2a3c47ffc427f308a3050025becfd75
Vansh Gupta <vanshsja1512@gmail.com>
2025-08-31 00:31:04 +0530

    Create DoublyLinkedList.c

---
+++ dsa/linked list/Doubly Linked List/DoublyLinkedList.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+void display(struct node *ptr)
+{
+    printf("NULL<->");
+    while(ptr!=NULL)
+    {
+        printf("%d<->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL");
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of nodes you want to create: ");
+    scanf("%d",&n);
+    struct node *head= NULL, *newNode= NULL, *current= NULL;
+    for(int i=0; i<n; i++)
+    {
+        int data;
+        printf("Enter data for %d node: ",i+1);
+        scanf("%d",&data);
+        newNode= (struct node*)malloc(sizeof(struct node));
+        newNode->data=data;
+        newNode->prev=NULL;
+        newNode->next=NULL;
+        if(head==NULL)
+        {
+            head=newNode;
+            current=newNode;
+        }
+        else
+        {
+            current->next=newNode;
+            newNode->prev=current;
+            current=newNode;
+        }
+    }
+    display(head);
+}
