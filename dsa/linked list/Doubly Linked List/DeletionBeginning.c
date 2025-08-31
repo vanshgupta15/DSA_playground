81f1f58418877cb7781c61275cc4ba93d0ebe523
Vansh Gupta <vanshsja1512@gmail.com>
2025-09-01 01:28:36 +0530

    Deletion beginning

---
+++ dsa/linked list/Doubly Linked List/DeletionBeginning.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+struct node * deletionBeginning(struct node *ptr)
+{
+    struct node *temp= malloc(sizeof(struct node));
+    temp=ptr;
+    ptr=ptr->next;
+    ptr->prev=NULL;
+    free(temp);
+    return ptr;
+}
+
+void display(struct node *ptr)
+{
+    printf("NULL<->");
+    while(ptr!=NULL)
+    {
+        printf("%d<->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL");
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of nodes you want to create: ");
+    scanf("%d",&n);
+    struct node *head= NULL, *newNode= NULL, *current= NULL;
+    for(int i=0; i<n; i++)
+    {
+        int data;
+        printf("Enter data for %d node: ",i+1);
+        scanf("%d",&data);
+        newNode= (struct node*)malloc(sizeof(struct node));
+        newNode->data=data;
+        newNode->prev=NULL;
+        newNode->next=NULL;
+        if(head==NULL)
+        {
+            head=newNode;
+            current=newNode;
+        }
+        else
+        {
+            current->next=newNode;
+            newNode->prev=current;
+            current=newNode;
+        }
+    }
+    printf("Old list:-\n");
+    display(head);
+    printf("\n");
+    head=deletionBeginning(head);
+    printf("New list:-\n");
+    display(head);
+}
