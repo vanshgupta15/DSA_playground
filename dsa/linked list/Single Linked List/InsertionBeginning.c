ad3265c1f2267199c2c9da17a15a8fb6412b318b
Vansh Gupta <vanshsja1512@gmail.com>
2025-11-09 17:26:23 +0530

    Update InsertionBeginning.c

--- dsa/linked list/Single Linked List/InsertionBeginning.c
+++ dsa/linked list/Single Linked List/InsertionBeginning.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node
 {
     int data;
     struct node *next;
 };
 
-void printing(struct node *ptr)
+void display(struct node *ptr)
 {
     while(ptr!=NULL)
     {
@@ -16,41 +17,55 @@ void printing(struct node *ptr)
     printf("NULL");
 }
 
+//Function to insert at beginning (same logic as your code)
+struct node* insertionBeginning(struct node *head)
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp = head;
+
+    struct node *addedNode = (struct node*)malloc(sizeof(struct node));
+    int newData;
+    printf("Enter data for new node: ");
+    scanf("%d", &newData);
+    addedNode->data = newData;
+    addedNode->next = temp;
+    head = addedNode;
+
+    return head;  // returning updated head
+}
+
 void main()
 {
     int n;
     printf("Enter the number of nodes you want to create: ");
     scanf("%d",&n);
-    struct node *head= NULL, *newNode= NULL, *current= NULL;
+
+    struct node *head = NULL, *newNode = NULL, *current = NULL;
     for(int i=0; i<n; i++)
     {
         int data;
         printf("Enter data for %d node: ",i+1);
         scanf("%d",&data);
-        newNode= (struct node*)malloc(sizeof(struct node));
-        newNode->data=data;
-        newNode->next=NULL;
-        if(head==NULL)
+
+        newNode = (struct node*)malloc(sizeof(struct node));
+        newNode->data = data;
+        newNode->next = NULL;
+
+        if(head == NULL)
         {
-            head=newNode;
-            current=newNode;
+            head = newNode;
+            current = newNode;
         }
         else
         {
-            current->next=newNode;
-            current=newNode;
+            current->next = newNode;
+            current = newNode;
         }
     }
 
-    //insertion at beginning
-    struct node *temp= (struct node*)malloc(sizeof(struct node));
-    temp=head;
-    struct node *addedNode= malloc(sizeof(struct node));
-    int newData;
-    printf("Enter data for new node:");
-    scanf("%d",&newData);
-    addedNode->data=newData;
-    addedNode->next=temp;
-    head=addedNode;
-    printing(head);
+    //Call function to insert at beginning
+    head = insertionBeginning(head);
+
+    //Print updated linked list
+    display(head);
 }
