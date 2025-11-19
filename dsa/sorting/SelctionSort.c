ec71cf8600e5d39f6edad970575990a1fe071764
Vansh Gupta <vanshsja1512@gmail.com>
2025-11-19 16:21:54 +0530

    Selection Sorting

---
+++ dsa/sorting/SelctionSort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void selectionSort(int arr[], int n) 
+{
+    int i, j, minIndex, temp;
+    for (i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) 
+        {
+            if (arr[j] < arr[minIndex]) 
+            {
+                minIndex = j;
+            }
+        }
+        temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void main() 
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    selectionSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+}
