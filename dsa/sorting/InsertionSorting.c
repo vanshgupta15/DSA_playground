7188c299e3d0173230bb676c77914bd71fdaf8dc
Vansh Gupta <vanshsja1512@gmail.com>
2025-11-19 17:50:25 +0530

    Insertion Sorting

---
+++ dsa/sorting/InsertionSorting.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) 
+{
+    int i, temp, j;
+    for (i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = temp;
+    }
+}
+int main() 
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
